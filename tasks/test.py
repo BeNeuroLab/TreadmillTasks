9fccbc99f0baf78d14a1fb49f16d23bd4479e836
mostafa.safaie <mostafa.safaie@gmail.com>
2022-02-14 13:31:05 +0000

    update test task

--- tasks/test.py
+++ tasks/test.py
@@ -12,7 +12,7 @@ import uarray
 
 states = ['intertrial']
 
-events = ['motion',
+events = ['motion','min',
           'session_timer']
 
 initial_state = 'intertrial'
@@ -22,8 +22,9 @@ initial_state = 'intertrial'
 # -------------------------------------------------------------------------
 
 # session params
-v.session_duration = 10 * second
-v.motion_timer___ = 10 * ms  # polls motion every 1ms
+v.session_duration = 1 * hour
+v.motion_timer___ = 60 * second  # polls motion every 1ms
+v.total=0
 
 
 # Run start and stop behaviour.
@@ -31,7 +32,9 @@ def run_start():
     # Code here is executed when the framework starts running.
     set_timer('session_timer', v.session_duration, True)
     hw.motionSensor.record()
-    # set_timer('motion', v.motion_timer___)
+    print(hw.motionSensor._threshold)
+    print(hw.motionSensor.threshold)
+    set_timer('min', v.motion_timer___)
 
 
 def run_end():
@@ -43,9 +46,10 @@ def run_end():
 
 # State behaviour functions.
 def intertrial(event):
-    if event == 'motion':
-        # print('mo')
-        pass
+    if event == 'min':
+        v.total += 1
+        print('t={}'.format(v.total))
+        set_timer('min', v.motion_timer___)
 
 
 # State independent behaviour.
@@ -54,8 +58,10 @@ def all_states(event):
     # irrespective of the state the machine is in.
     if event == 'motion':
         # read the motion registers and and append the variables
-        print('dX={}; dY={}'.format(hw.motionSensor.x / hw.motionSensor.sensor.CPI * 2.54, hw.motionSensor.y / hw.motionSensor.sensor.CPI * 2.54))
+        print('dX={}; dY={}'.format(hw.motionSensor.x/ hw.motionSensor.sensor.CPI * 2.54, hw.motionSensor.y/ hw.motionSensor.sensor.CPI * 2.54))
+        v.total += hw.motionSensor.threshold
         # set_timer('motion', v.motion_timer___)
 
     elif event == 'session_timer':
+        print("total={}".format(v.total))
         stop_framework()
