8c59ad5a2721b1b1da5d8544d45ad1b4ac450d7c
mostafa.safaie <mostafa.safaie@gmail.com>
2022-02-23 15:00:39 +0000

    update odour ports based on new plan

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -10,11 +10,11 @@ motionSensor = MotionDetector_2ch(name='MotSen1', event='motion',
 
 # in each direction, Odour0 is always the clean air, Odour1 is the odourant,...
 # odourDelivery = ParallelOdourRelease(5, 2,
-#                                      board.port_1.DIO_C, board.port_1.POW_A,    # Dir1
-#                                      board.port_1.POW_B, board.port_4.DIO_A,    # Dir2
-#                                      board.port_4.DIO_B, board.port_4.DIO_C,    # Dir3
-#                                      board.port_5.DIO_A, board.port_5.DIO_B,    # Dir4
-#                                      board.port_5.POW_A, board.port_5.POW_B)    # Dir5
+#                                      board.port_1.POW_A, board.port_1.POW_B,    # Dir1
+#                                      board.port_2.POW_A, board.port_2.POW_B,    # Dir2
+#                                      board.port_3.POW_A, board.port_3.POW_B,    # Dir3
+#                                      board.port_4.DIO_A, board.port_4.DIO_B,    # Dir4
+#                                      board.port_4.POW_A, board.port_4.POW_B)    # Dir5
 
 # lickometer = Lickometer(port=board.port_6, rising_event_A='lick', falling_event_A='lick_off',
 #                         rising_event_B='_lick_2___', falling_event_B='_lick_2_off___', debounce=5)
