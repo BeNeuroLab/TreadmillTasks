011ae05fd9b69ac298240c9bc4843b8a79e5e5b9
mostafa.safaie <mostafa.safaie@gmail.com>
2022-02-23 15:00:58 +0000

    uncomment more devices

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -9,16 +9,16 @@ motionSensor = MotionDetector_2ch(name='MotSen1', event='motion',
                                   sensor2pins={'CS': board.port_1.DIO_C, 'MI': board.port_2.DIO_A, 'MO': board.port_2.DIO_B, 'SCK': board.port_3.DIO_C, 'reset': board.port_4.DIO_C})
 
 # in each direction, Odour0 is always the clean air, Odour1 is the odourant,...
-# odourDelivery = ParallelOdourRelease(5, 2,
-#                                      board.port_1.POW_A, board.port_1.POW_B,    # Dir1
-#                                      board.port_2.POW_A, board.port_2.POW_B,    # Dir2
-#                                      board.port_3.POW_A, board.port_3.POW_B,    # Dir3
-#                                      board.port_4.DIO_A, board.port_4.DIO_B,    # Dir4
-#                                      board.port_4.POW_A, board.port_4.POW_B)    # Dir5
+odourDelivery = ParallelOdourRelease(5, 2,
+                                     board.port_1.POW_A, board.port_1.POW_B,    # Dir1
+                                     board.port_2.POW_A, board.port_2.POW_B,    # Dir2
+                                     board.port_3.POW_A, board.port_3.POW_B,    # Dir3
+                                     board.port_4.DIO_A, board.port_4.DIO_B,    # Dir4
+                                     board.port_4.POW_A, board.port_4.POW_B)    # Dir5
 
-# lickometer = Lickometer(port=board.port_6, rising_event_A='lick', falling_event_A='lick_off',
-#                         rising_event_B='_lick_2___', falling_event_B='_lick_2_off___', debounce=5)
+lickometer = Lickometer(port=board.port_6, rising_event_A='lick', falling_event_A='lick_off',
+                        rising_event_B='_lick_2___', falling_event_B='_lick_2_off___', debounce=5)
 
-# rewardSol = lickometer.SOL_1  # has two methods: on() and off()
+rewardSol = lickometer.SOL_1  # has two methods: on() and off()
 
-# speaker = Audio_board(board.port_7)
+speaker = Audio_board(board.port_7)
