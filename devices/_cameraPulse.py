cea82c582abdcf84c1d65e92e1ed23e310e42e94
AtMostafa <mostafa.safaie@gmail.com>
2023-06-30 16:39:39 +0100

    copy from new PyC code

--- devices/_cameraPulse.py
+++ devices/_cameraPulse.py
@@ -1,17 +1,32 @@
-import pyb, machine, time
-import pyControl.hardware as _h
+import pyb
+import pyControl.hardware as hw
+import pyControl.utility as ut
 
-class CameraPulse(_h.Digital_output):
-    "generate a clock to trigger the cameras"
-    def __init__(self, pin, trigger_rate=100, duty_cycle=50):
-        super().__init__(pin, pulse_enabled=True)
-        self.duty_cycle = duty_cycle
-        self.trigger_rate = trigger_rate
+class CameraPulse(hw.IO_object):
 
-    def start(self):
-        "start generating the trigger pulse"
-        self.pulse(freq=self.trigger_rate, duty_cycle=self.duty_cycle)
+    def __init__(self, pin, pulse_rate, name='frame_trigger'):
+        self.timer = pyb.Timer(hw.available_timers.pop())
+        self.pin = pyb.Pin(pin, mode=pyb.Pin.OUT, value=0)
+        self.name = name
+        self.pulse_rate = pulse_rate
+        self.analog_channel = hw.Analog_channel(name, pulse_rate, plot=False)
+        hw.assign_ID(self)
 
-    def stop(self):
-        "stop generating the trigger pulse"
-        self.off()
+    def _run_start(self):
+        self.pulse_n = 0
+        self.pinstate = False
+        self.pin.value(False)
+        self.timer.init(freq=2*self.pulse_rate) # Set timer to trigger subsequent pulses.
+        self.timer.callback(self.ISR)
+        ut.print('{} outputting pulses at {}Hz'.format(self.name, self.pulse_rate))
+
+    def _run_stop(self):
+        self.pin.value(False)
+        self.timer.deinit()
+
+    def ISR(self,t):
+        self.pinstate = not self.pinstate
+        self.pin.value(self.pinstate)
+        if self.pinstate:
+            self.pulse_n += 1
+            self.analog_channel.put(self.pulse_n)
