ee0b9917682b662f525ba5e03aec44bd2bec25a7
AtMostafa <mostafa.safaie@gmail.com>
2023-06-30 16:35:42 +0100

    change LED pins

--- devices/LEDStim.py
+++ devices/LEDStim.py
@@ -7,11 +7,11 @@ class LEDStim:
     def __init__(self):
         """PINS should be exactly `Ndirections` strings"""
         self.n_directions = 5
-        pins = ('W60',    # Dir0
-                'W22',    # Dir1
-                'W45',    # Dir2
-                'W43',    # Dir3
-                'W24')    # Dir4
+        pins = ('W45',    # Dir0
+                'W43',    # Dir1
+                'W24',    # Dir2
+                'W10',    # Dir3
+                'W68')    # Dir4
         powerlines = ('W16', 'W50', 'W60', 'W22')  # this variable indicates the POW pins used so that their logic level is inverted automatically.
 
         for d in range(self.n_directions):
