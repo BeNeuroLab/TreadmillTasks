10212952f0ddd056b65fde194b7a199cead91266
AtMostafa <mostafa.safaie@gmail.com>
2023-07-19 15:03:31 +0100

    trigger reward by lick

--- tasks/0-lick-triggered-reward.py
+++ tasks/0-lick-triggered-reward.py
@@ -18,8 +18,6 @@ events = ['motion',
           'lick',
           'lick_off',
           'session_timer',
-          'IT_timer',
-          'max_IT_timer',
           'stim_timer',
           'reward_timer',
           ]
@@ -47,23 +45,7 @@ v.y___ = 0
 
 # trial params
 v.led_direction = -1
-v.trial_start_len = 100 * ms
-
-# -------------------------------------------------------------------------
-# State-independent Code
-# -------------------------------------------------------------------------
-
-
-def cue_random_led(LedDevice: LEDStim):
-    """
-    Cues 1 LED at a random direction
-    """
-    stim_dir = randint(0, LedDevice.n_directions - 1)
-    LedDevice.all_off()
-    LedDevice.cue_led(stim_dir)
-    print('{}, LED_direction'.format(stim_dir))
-
-    return stim_dir
+v.trial_start_len = 1 * second
 
 
 # -------------------------------------------------------------------------
@@ -76,7 +58,6 @@ def run_start():
     "Code here is executed when the framework starts running."
     set_timer('session_timer', v.session_duration, True)
     hw.motionSensor.record()
-    hw.LED_Delivery.all_off()
     print('CPI={}'.format(hw.motionSensor.sensor_x.CPI))
 
 
@@ -95,21 +76,9 @@ def intertrial(event):
     "intertrial state behaviour"
     if event == 'entry':
         # coded so that at this point, there is clean air coming from every direction
-        set_timer('IT_timer', v.min_IT_duration)
-        hw.LED_Delivery.all_off()
-        v.IT_duration_done___ = False
-        v.IT_distance_done___ = False
         hw.motionSensor.threshold = v.min_IT_movement # to issue an event only after enough movement
-    elif event == 'exit':
-        disarm_timer('IT_timer')
-    elif event == 'IT_timer':
-        v.IT_duration_done___ = True
-        if v.IT_distance_done___:
-            goto_state('trial_start')
-    elif event == 'motion':
-        v.IT_distance_done___ = True
-        if v.IT_duration_done___:
-            goto_state('trial_start')
+    elif event == 'lick':
+        goto_state('reward')
 
 
 def trial_start(event):
@@ -118,14 +87,12 @@ def trial_start(event):
         v.trial_number += 1
         print('{}, trial_number'.format(v.trial_number))
         hw.LED_Delivery.all_off()
-        timed_goto_state('reward', v.trial_start_len)
-
+        timed_goto_state('intertrial', v.trial_start_len)  # enforcing min 1s between rewards
 
 
 def reward(event):
     "reward state"
     if event == 'entry':
-        v.led_direction = cue_random_led(hw.LED_Delivery)
         set_timer('reward_timer', v.reward_duration, False)
         hw.rewardSol.on()
         print('{}, reward_on'.format(get_current_time()))
@@ -133,7 +100,7 @@ def reward(event):
         disarm_timer('reward_timer')
     elif event == 'reward_timer':
         hw.rewardSol.off()
-        goto_state('intertrial')
+        goto_state('trial_start')
 
 
 # State independent behaviour.
@@ -148,9 +115,6 @@ def all_states(event):
         v.x___ = hw.motionSensor.x #/ hw.motionSensor.sensor_x.CPI * 2.54
         v.y___ = hw.motionSensor.y #/ hw.motionSensor.sensor_x.CPI * 2.54
         print('{},{}, dM'.format(v.x___, v.y___))
-    elif event == 'lick':
-        #TODO: handle the lick data better
-        pass
     elif event == 'session_timer':
         hw.motionSensor.stop()
         stop_framework()
