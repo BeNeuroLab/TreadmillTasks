aa136fedbd082e13662f3901111c8574501a89ac
Mostafa Safaie <mostafa.safaie@gmail.com>
2023-08-21 15:42:56 +0100

    change to `pyb.Timer`

--- devices/_reward.py
+++ devices/_reward.py
@@ -1,25 +1,25 @@
-import machine
+import pyb
 import pyControl.hardware as _h
 
 class Reward(_h.IO_object):
     "Open the solnoid to release water reward"
     def __init__(self, sol:_h.Digital_output, reward_duration: int=100):
         "reward_duration in ms"
-        self.reward_duration = int(reward_duration)
+        self.reward_freq = int(1000 / reward_duration)
         self.sol = sol
         self.sol.off()
         self.sol_state = False  # to avoid turning on the solenoid twice
-        self.timer = machine.Timer(_h.available_timers.pop())
+        self.timer = pyb.Timer(_h.available_timers.pop())
 
     def release(self):
         "release water reward"
         if self.sol_state is False:
             self.sol.on()
             self.sol_state = True
-            self.timer.init(period=self.reward_duration, mode=machine.Timer.ONE_SHOT, callback=self.callback)
+            self.timer.init(freq=self.reward_freq, callback=self._callback)
 
     def _callback(self, timer):
-        "start generating the trigger pulse"
+        "turn off the sol"
         self.sol.off()
         self.sol_state = False
     
