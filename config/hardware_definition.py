2d3f21cba70a6b85727a471fec9444c182941b13
Mostafa Safaie <mostafa.safaie@gmail.com>
2023-11-16 17:08:29 +0000

    Update hardware_definition.py

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -13,11 +13,11 @@ motionSensor = MotionDetector(name='MotSen1', event='motion',
 
 LED_Delivery = LEDStim()
 
-lickometer = Lickometer(port=board.port_6, rising_event_A='lick', falling_event_A='lick_off',
-                        rising_event_B='_lick_2___', falling_event_B='_lick_2_off___', debounce=5)
+lickometer = Lickometer(lick_port=board.port_6, sol_port=board.port_4, rising_event_A='lick', debounce=5)
 reward = Reward(lickometer.SOL_1 , reward_duration=50)
 
 audio = AudioStim(board.port_11)
+speaker = Audio_board(board.port_7) 
 
 cameraTrigger = CameraPulse(pin=board.port_1.POW_B, trigger_rate=100, duty_cycle=50)
 
