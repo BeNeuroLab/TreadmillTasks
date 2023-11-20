20a7b0557447c2b9ddee680f076ac16707e15c0b
Mostafa Safaie <mostafa.safaie@gmail.com>
2023-11-20 16:51:57 +0000

    no reward leak

--- devices/_lickometer.py
+++ devices/_lickometer.py
@@ -5,4 +5,4 @@ class Lickometer():
     def __init__(self,  lick_port, sol_port, rising_event_A='lick_1', debounce=5, **kwargs):
         self.lick_1 = Digital_input(lick_port.DIO_A, rising_event_A, falling_event=None, debounce=debounce)
         self.SOL_1  = Digital_output(sol_port.POW_B)
-        self.SOL_2  = Digital_output(sol_port.POW_A)
+        self.SOL_1.off()
