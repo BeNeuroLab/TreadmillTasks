164dc6a814a2ae744b287d9d196fad251e3a15c6
mostafa <mostafa.safaie@gmail.com>
2024-01-22 11:39:59 +0000

    add `self.speakers`

--- devices/AudioStim.py
+++ devices/AudioStim.py
@@ -10,29 +10,29 @@ class AudioStim(Audio_player):
         self.set_volume(25)  # Between 1 - 30
         utime.sleep_ms(20)  # wait for the audio player to be ready
 
-        pins = ('W10',    # Dir0
-                'W68',    # Dir1
-                'W66',    # Dir2
-                'W58',    # Dir3
-                'W56',    # Dir4
-                'W64',    # Dir5
-                'W62')    # Dir6
+        pins = {0:'W10',    # Dir0
+                1:'W68',    # Dir1
+                2:'W66',    # Dir2
+                3:'W58',    # Dir3
+                4:'W56',    # Dir4
+                5:'W64',    # Dir5
+                6:'W62'}    # Dir6
         #the POW pins used so that their logic level is inverted automatically.
         powerlines = ('W23', 'W25', 'W62', 'W64','W30','W32')
+        self.speakers = {}
 
-        for d in range(self.n_directions):
-            spk = 'spk' + str(d)
-            setattr(self, spk, _h.Digital_output(pin=pins[d], inverted=pins[d] in powerlines))
-            getattr(self, spk).off()
+        for direction, pin in pins.items():
+            self.speakers[direction] = _h.Digital_output(pin=pin, inverted=pin in powerlines)
+            self.speakers[direction].off()
 
         # Start playing the audio file
-        self.play(folder_num = 1, file_num = 3)  # Play file 1 from folder 1.
+        self.play(folder_num = 1, file_num = 3)  # Play file 1 from folder 3.
         utime.sleep_ms(20)  # wait for the audio player to be ready
 
     def start(self):
         "start playing"
         self.all_off()
-        self.command(0x0D)
+        self.command(0x0D)  # start the playback
 
     def stop(self):
         "stop the playback"
@@ -41,29 +41,26 @@ class AudioStim(Audio_player):
 
     def all_off(self):
         "turn off all Speakers"
-        for d in range(self.n_directions):
-            spk = 'spk' + str(d)
-            getattr(self, spk).off()
+        for spk in self.speakers.values():
+            spk.off()
 
     def all_on(self):
         "turn on all the Speakers"
-        for d in range(self.n_directions):
-            spk = 'spk' + str(d)
-            getattr(self, spk).on()
+        for spk in self.speakers.values():
+            spk.on()
+
 
     def cue(self, direction:int):
         "turn on the Speaker corresponding to the given direction"
         assert direction < self.n_directions, 'direction is too big'
-        for d in range(self.n_directions):
-            spk = 'spk' + str(d)
+        for d, spk in self.speakers.items():
             if d == direction:
-                getattr(self, spk).on()
+                spk.on()
             else:
-                getattr(self, spk).off()
+                spk.off()
 
     def cue_array(self, arr:list):
         "turn on the all the speakers corresponding to the given directions in `arr`"
         self.all_off()
         for d in arr:
-            spk = 'spk' + str(d)
-            getattr(self, spk).on()
+            self.speakers[d].on()
