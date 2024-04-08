1823b7413e09437c4399c65b74d2592ebd2ba067
mostafa <mostafa.safaie@gmail.com>
2024-04-08 11:18:26 +0100

    longer timeout

--- tasks/M032/2-rand-spk.py
+++ tasks/M032/2-rand-spk.py
@@ -111,7 +111,7 @@ def trial(event):
         print('{}, led_direction'.format(v.next_led___))
         set_timer('spk_update', choice(v.sound_bins), False)
     elif event == 'lick':  # lick during the trial delays the sweep
-        reset_timer('spk_update', v.sound_bins[1], False)
+        reset_timer('spk_update', v.sound_bins[-1], False)
         reset_timer('trial_timeout', 20 * second, False)
     elif event == 'spk_update':
         if hw.sound.active == hw.light.active:  # speaker lines up with LED
