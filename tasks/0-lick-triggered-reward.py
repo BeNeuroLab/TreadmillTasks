65742060d4e4e2bc9a058e618a46094e88470c56
AtMostafa <mostafa.safaie@gmail.com>
2024-07-04 11:55:52 +0100

    redid task

--- tasks/0-lick-triggered-reward.py
+++ tasks/0-lick-triggered-reward.py
@@ -1,9 +1,9 @@
-# PyTreadmillTask
+# reward on lick, half of the time, when sound and light match
 
 from pyControl.utility import *
 import hardware_definition as hw
 from devices import *
-import math
+import utime
 
 
 # -------------------------------------------------------------------------
@@ -13,6 +13,7 @@ states = ['intertrial',
 
 events = ['lick',
           'session_timer',
+          'spk_update',
           'motion']
 
 initial_state = 'trial'
@@ -22,37 +23,76 @@ initial_state = 'trial'
 v.session_duration = 20 * minute
 v.reward_duration = 30 * ms
 v.trial_number = 0
-
-# intertrial params
 v.min_IT_movement___ = 10  # cm - must be a multiple of 5
-v.x___ = 0
-v.y___ = 0
+v.sound_bins = (0.5 * second, 1 * second, 1.5 * second)
 
-# trial params
 v.trial_len = 5 * second
 v.led_len = 500 * ms
+
+v.last_spk___ = 1
+v.next_spk___ = 0
+
+v.spks___ = [0, 1, 2, 3, 4, 5, 6]
+v.leds___ = [1, 2, 3, 4, 5]
+
+
+# -------------------------------------------------------------------------
+def switch_leds():
+    """
+    switch which half of LEDs to turn on on each call
+    """
+    if v.leds___[0] in hw.light.active:
+        out = v.leds___[:3]
+    elif v.leds___[-1] in hw.light.active:
+        out = v.leds___[-3:]
+    else:
+        out = v.leds___[:3]
+
+    print('{}, led_direction'.format(out))
+    hw.light.cue_array(out)
+
+def next_spk():
+    """
+    returns the next speakers, in either direction of the sweep
+    """
+    assert len(hw.sound.active)==1, 'one speaker can be active'
+    active_spk = hw.sound.active[0]
+    active_spk_idx = v.spks___.index(active_spk)
+
+    if active_spk > v.last_spk___:
+        if active_spk < v.spks___[-1]:
+            out = active_spk_idx + 1
+        else:
+            out = active_spk_idx - 1
+            switch_leds()
+    else:
+        if active_spk > v.spks___[0]:
+            out = active_spk_idx - 1
+        else:
+            out = active_spk_idx + 1
+            switch_leds()
+
+    v.last_spk___ = active_spk
+
+    return v.spks___[out]
 
 
 # -------------------------------------------------------------------------
 def run_start():
     "Code here is executed when the framework starts running."
     hw.sound.set_volume(15)  # Between 1 - 30
-    utime.sleep_ms(20)  # wait for the audio player to be ready
+    utime.sleep_ms(20)  # wait for the sound player to be ready
     hw.motionSensor.record()
-    hw.motionSensor.threshold = v.min_IT_movement___
+    hw.motionSensor.threshold = 10
     hw.sound.start()
     hw.light.all_off()
-    hw.reward.reward_duration = v.reward_duration
     set_timer('session_timer', v.session_duration, True)
     print('{}, CPI'.format(hw.motionSensor.sensor_x.CPI))
     print('{}, before_camera_trigger'.format(get_current_time()))
     hw.cameraTrigger.start()
 
 def run_end():
-    """ 
-    Code here is executed when the framework stops running.
-    Turn off all hardware outputs.
-    """
+    "Code here is executed when the framework stops running."
     hw.light.all_off()
     hw.reward.stop()
     hw.motionSensor.off()
@@ -64,32 +104,35 @@ def run_end():
 
 # -------------------------------------------------------------------------
 def trial(event):
-    "start state behaviour"
+    "led at first, and spk update at later bins"
     if event == 'entry':
-        hw.light.cue(3)
-    if event == 'lick':
-        hw.sound.cue(3)
-        timed_goto_state('reward', v.led_len)
+        hw.sound.cue(v.next_spk___)
+        print('{}, spk_direction'.format(v.next_spk___))
+        set_timer('spk_update', choice(v.sound_bins), False)
+    elif event == 'lick':  # lick during the trial delays the sweep
+        if hw.sound.active[0] in hw.light.active:  # speaker lines up with LED
+            goto_state('reward')  # reward
+    elif event == 'spk_update':
+        set_timer('spk_update', choice(v.sound_bins), False)
+    elif event == 'exit':
+        disarm_timer('spk_update')
 
-def reward(event):
-    "gap for the light cue"
+def reward (event):
+    "reward state"
     if event == 'entry':
+        timed_goto_state('trial', v.sound_bins[-1])
         hw.reward.release()
-        print('{}, reward_number'.format(v.trial_number))
-        timed_goto_state('intertrial', v.led_len)
-
-def intertrial(event):
-    "intertrial"
-    if event == 'entry':
-        hw.light.all_off()
-        hw.sound.all_off()
-        v.trial_number += 1
-        timed_goto_state('trial', v.trial_len)  # enforcing min 3s between rewards
+        v.reward_number += 1
+        print('{}, reward_number'.format(v.reward_number))
 
 
 def all_states(event):
     """
     Executes before the state code.
     """
-    if event == 'session_timer':
+    if event == 'spk_update':
+        spk_dir = next_spk()
+        print('{}, spk_direction'.format(spk_dir))
+        hw.sound.cue(spk_dir)
+    elif event == 'session_timer':
         stop_framework()
