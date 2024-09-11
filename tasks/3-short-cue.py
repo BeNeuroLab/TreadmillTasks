82eec46812f85d665a8573597a9cda5ebcc3fabc
unknown <cecilia.gallegocarracedo@gmail.com>
2024-09-11 18:19:37 +0100

    added timeout to task3

--- tasks/3-short-cue.py
+++ tasks/3-short-cue.py
@@ -11,11 +11,13 @@ from devices import *
 
 states = ['trial',
           'led_on',
-          'reward']
+          'reward',
+          'timeout']
 
 events = ['lick',
           'motion',
           'spk_update',
+          'led_timer',
           'session_timer']
 
 initial_state = 'trial'
@@ -26,6 +28,8 @@ v.session_duration = 45 * minute
 v.reward_duration = 30 * ms
 v.sound_bins = (0.5 * second, 0.6 * second, 0.7 * second, 1.5 * second)
 v.offlick_penalty = 5 * second
+v.target_duration = 1 * minute
+v.timeout_duration = 20 * second
 
 v.reward_number = 0
 v.IT_duration = 5 * second
@@ -77,6 +81,7 @@ def run_start():
     print('{}, CPI'.format(hw.motionSensor.sensor_x.CPI))
     print('{}, before_camera_trigger'.format(get_current_time()))
     hw.cameraTrigger.start()
+    set_timer('led_timer',v.target_duration)
 
 def run_end():
     "Code here is executed when the framework stops running."
@@ -100,11 +105,14 @@ def trial(event):
         v.next_led___ = choice(v.leds___)
     elif event == 'lick':  # lick during the trial delays the sweep
         reset_timer('spk_update', v.offlick_penalty)
+        reset_timer('led_timer',v.target_duration)
     elif event == 'spk_update':
         if hw.sound.active[0] == v.next_led___:
             goto_state('led_on')
         else:
-            set_timer('spk_update', choice(v.sound_bins), False)
+            set_timer('spk_update', choice(v.sound_bins), False)    
+    elif event == 'led_timer':
+        goto_state('timeout')
     elif event == 'exit':
         disarm_timer('spk_update')
 
@@ -113,10 +121,13 @@ def led_on (event):
     if event == 'entry':
         hw.light.cue(v.next_led___)
         print('{}, led_direction'.format(v.next_led___))
+        pause_timer('led_timer')
         timed_goto_state('trial', v.sound_bins[-1])
         v.next_spk___ = next_spk()  # in case of no lick, sweep continues
     elif event == 'lick':
         goto_state('reward')
+    elif event == 'exit':
+        unpause_timer('led_timer')
 
 def reward (event):
     "intertrial state"
@@ -124,9 +135,20 @@ def reward (event):
         hw.reward.release()
         v.reward_number += 1
         print('{}, reward_number'.format(v.reward_number))
+        reset_timer('led_timer',v.target_duration)
         timed_goto_state('trial', v.IT_duration)
 
-
+def timeout(event):
+    "timeout state"
+    if event == 'entry':
+        v.next_led___ = choice(v.leds___)
+        hw.light.all_off()
+        hw.sound.all_off()
+        v.next_spk___ = v.spks___[0]
+        timed_goto_state('trial', v.timeout_duration)
+    elif event == 'exit':
+        reset_timer('led_timer',v.target_duration)
+        
 def all_states(event):
     """
     Executes before the state code.
