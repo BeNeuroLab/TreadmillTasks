3f0f64858052b3718aeee607b79b0665fef019b4
Mostafa Safaie <mostafa.safaie@gmail.com>
2024-10-25 11:59:11 +0100

    fix typo

--- devices/_lickometer.py
+++ devices/_lickometer.py
@@ -17,6 +17,6 @@ class Lickometer():
     
     def __init__(self,  lick_port, sol_port, rising_event_A='lick_1', debounce=5, **kwargs):
         # Event triggered when pin is pulled down
-        self.lick_1 = Digital_input(lick_port.DIO_A, rising_event_A, falling_event=None, debounce=debounce, pull=up)
+        self.lick_1 = Digital_input(lick_port.DIO_A, rising_event_A, falling_event=None, debounce=debounce, pull='up')
         self.SOL_1  = Digital_output(sol_port.POW_A)
         self.SOL_1.off()
