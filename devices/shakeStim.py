628a65fe8c9b36ce9984f68ab3013f4963a64a61
Mostafa Safaie <mostafa.safaie@gmail.com>
2024-11-06 11:06:16 +0000

    working relay

--- devices/shakeStim.py
+++ devices/shakeStim.py
@@ -8,14 +8,18 @@ class shakeStim:
         """PINS should be exactly `Ndirections` strings"""
 
         self.sol_0 = _h.Digital_output(pin=port_exp.port_6.DIO_A)
-        self.sol_1 = _h.Digital_output(pin=port_exp.port_6.DIO_B)
-        self.sol_2 = _h.Digital_output(pin=port_exp.port_6.POW_A, inverted=True)
-        self.sol_3 = _h.Digital_output(pin=port_exp.port_6.POW_B, inverted=True)
-        self.sol_4 = _h.Digital_output(pin=port_exp.port_4.DIO_A)
-        self.sol_5 = _h.Digital_output(pin=port_exp.port_4.DIO_B)
-        self.sol_6 = _h.Digital_output(pin=port_exp.port_4.POW_A, inverted=True)
-        self.sol_7 = _h.Digital_output(pin=port_exp.port_4.POW_B, inverted=True)
-        # self.sol_1.off()
+        # self.sol_1 = _h.Digital_output(pin=port_exp.port_6.DIO_B)
+        self.sol_1 = _h.Digital_output(pin=port_exp.port_6.POW_A, inverted=True)
+        self.sol_2 = _h.Digital_output(pin=port_exp.port_6.POW_B, inverted=True)
+        self.sol_3 = _h.Digital_output(pin=port_exp.port_3.DIO_A)
+        self.sol_4 = _h.Digital_output(pin=port_exp.port_3.DIO_B)
+        self.sol_5 = _h.Digital_output(pin=port_exp.port_3.POW_A, inverted=True)
+        self.sol_6 = _h.Digital_output(pin=port_exp.port_3.POW_B, inverted=True)
+
+        self.sol_7 = _h.Digital_output(pin=port_exp.port_4.DIO_A)
+        self.sol_8 = _h.Digital_output(pin=port_exp.port_4.DIO_B)
+        self.sol_9 = _h.Digital_output(pin=port_exp.port_4.POW_A, inverted=True)
+        self.sol_10 = _h.Digital_output(pin=port_exp.port_4.POW_B, inverted=True)
 
     def all_off(self):
         "turn off all sols"
--- tasks/tests/test_solenoids.py
+++ tasks/tests/test_solenoids.py
@@ -34,17 +34,24 @@ v.min_motion = 15
 v.sol_duration = 1 * second
 v.sol_number = 0
 v.intertrial_duration = 1 * second
-v.trial_duration = 5 * second
+v.trial_duration = 3 * second
 v.session_duration = 10 * minute
-v.max_solenoids = 8
+v.max_solenoids = 11
+
+# def run_end():
+#     # Code here is executed when the framework stops running.
+#     # Turn off all hardware outputs.
+#     hw.motionSensor.off()
+#     hw.off()
 
 # States and transitions
 def intertrial(event):
 
     if event == 'entry':
-        set_timer('intertrial_timer', v.intertrial_duration, True)
         earthquake_stim.sol_off(v.sol_number)
         v.sol_number = v.sol_number + 1
+        set_timer('intertrial_timer', v.intertrial_duration, True)
+        
 
     elif event == 'intertrial_timer':
         goto_state('trial')
@@ -53,15 +60,16 @@ def intertrial(event):
 
 
 def trial(event):
-    if v.sol_number == v.max_solenoids:
-        hw.motionSensor.stop()
-        stop_framework()
 
-    set_timer('trial_timer', v.trial_duration, True)
+    if event == 'entry':
+        if v.sol_number == v.max_solenoids:
+            stop_framework()
+        set_timer('trial_timer', v.trial_duration, True)
+
 
     if event == 'trial_timer':
+        print('{}, Sol_number'.format(v.sol_number))
         earthquake_stim.sol_on(v.sol_number)
-        
         timed_goto_state('intertrial', v.sol_duration)
 
     # if event == 'entry':
