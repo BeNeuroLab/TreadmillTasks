8388e867d154c7ca00ecd701d774fe245bc77ea4
gen21kawa <genji.kawakita@gmail.com>
2025-02-10 10:51:48 +0000

    updated audio stim and hw def for freq task

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -14,11 +14,11 @@ bci_link = UARTlink('cursor_update', timer_freq=100)
 light = LEDStim()
 light.all_off()
 
-lickometer = Lickometer(lick_port=board.port_6, sol_port=board.port_7, rising_event_A='lick', debounce=5,pull="up")
+lickometer = Lickometer(lick_port=board.port_6, sol_port=board.port_4, rising_event_A='lick', debounce=5,pull="up")
 reward = Reward(lickometer.SOL_1 , reward_duration=50)
 
 # sound = AudioStim(board.port_11)
-speaker = Audio_board(board.port_8)
+speaker = Audio_board(board.port_7)
 
 cameraTrigger = CameraPulse(pin=board.port_1.POW_B, trigger_rate=100, duty_cycle=50)
 
