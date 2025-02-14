79ada73d8cc63d5d650d55da998b3398eca94416
gen21kawa <genji.kawakita@gmail.com>
2025-02-14 16:16:24 +0000

    updated to follow task conventions

--- tasks/1-two-freq.py
+++ tasks/1-two-freq.py
@@ -5,13 +5,13 @@ from devices import *
 # -------------------------------------------------------------------------
  
 # States
-states = ['wait_for_trial', 'stimulus_on', 'reward', 'silence']
+states = ['intertrial', 'stimulus_on', 'reward', 'timeout']
  
 # Events
-events = ['session_timer', 'lick', 'stimulus_timer', 'silence_timer']
+events = ['session_timer', 'lick', 'stimulus_timer', 'timeout_timer']
  
 # Initial state
-initial_state = 'wait_for_trial'
+initial_state = 'intertrial'
 
 # -------------------------------------------------------------------------
  
@@ -25,7 +25,7 @@ v.correct_trials = 0
 v.total_trials = 0
 
 v.target_duration = 40 * second
-v.silence_duration = 15 * second
+v.timeout_duration = 15 * second
 
 # -------------------------------------------------------------------------
  
@@ -35,7 +35,7 @@ def run_start():
     hw.cameraTrigger.start()
     set_timer('session_timer', v.session_duration)
     print('Session started')
-    set_timer('silence_timer',v.target_duration)
+    set_timer('timeout_timer',v.target_duration)
  
 def run_end():
     hw.speaker.off()
@@ -43,14 +43,14 @@ def run_end():
     hw.cameraTrigger.stop()
     print('Session ended. Correct trials: {}/{}'.format(v.correct_trials, v.total_trials))
  
-def wait_for_trial(event):
+def intertrial(event):
     if event == 'entry':
         hw.speaker.off()
         timed_goto_state('stimulus_on', v.iti_duration)
     elif event == 'lick':
-        reset_timer('silence_timer',v.target_duration)
-    elif event == 'silence_timer':
-        goto_state('silence')
+        reset_timer('timeout_timer',v.target_duration)
+    elif event == 'timeout_timer':
+        goto_state('timeout')
  
 def stimulus_on(event):
     if event == 'entry':
@@ -60,27 +60,27 @@ def stimulus_on(event):
         set_timer('stimulus_timer', v.stimulus_duration)
     elif event == 'lick':
         goto_state('reward')
-    elif event == 'silence_timer':
-        goto_state('silence')
+    elif event == 'timeout_timer':
+        goto_state('timeout')
     elif event == 'stimulus_timer':
         hw.speaker.off()
-        goto_state('wait_for_trial')
+        goto_state('intertrial')
         
 def reward(event):
     if event == 'entry':
         hw.reward.release()
         v.correct_trials += 1
-        print('Correct! Trials: {}/{}'.format(v.correct_trials, v.total_trials))
-        timed_goto_state('wait_for_trial', 1000)
-        reset_timer('silence_timer',v.target_duration)
+        print('reward number {}/{} ({})'.format(v.correct_trials, v.total_trials, v.spk_freqs))
+        timed_goto_state('intertrial', 1000)
+        reset_timer('timeout_timer',v.target_duration)
         
-def silence(event):
-    "silence state"
+def timeout(event):
+    "timeout state"
     if event == 'entry':
         hw.speaker.off()
-        timed_goto_state('wait_for_trial', v.silence_duration)
+        timed_goto_state('intertrial', v.timeout_duration)
     elif event == 'exit':
-        reset_timer('silence_timer',v.target_duration)
+        reset_timer('timeout_timer',v.target_duration)
  
 def all_states(event):
     if event == 'session_timer':
--- tasks/2-two-freq-sweep.py
+++ tasks/2-two-freq-sweep.py
@@ -6,13 +6,13 @@ import random
 # -------------------------------------------------------------------------
  
 # States
-states = ['wait_for_trial', 'stimulus_on', 'reward', 'silence', 'reward_freq']
+states = ['intertrial', 'stimulus_on', 'reward', 'timeout', 'reward_freq']
  
 # Events
-events = ['session_timer', 'sweep_timer', 'lick', 'stimulus_timer', 'silence_timer']
+events = ['session_timer', 'sweep_timer', 'lick', 'stimulus_timer', 'timeout_timer']
  
 # Initial state
-initial_state = 'wait_for_trial'
+initial_state = 'intertrial'
 
 # -------------------------------------------------------------------------
  
@@ -30,7 +30,7 @@ v.correct_trials = 0
 v.total_trials = 0
 
 v.target_duration = 40 * second
-v.silence_duration = 15 * second
+v.timeout_duration = 15 * second
 
 # Define frequency progression
 mid_idx = len(v.spk_freqs) // 2  # Index of middle frequency
@@ -54,7 +54,7 @@ def run_start():
     hw.cameraTrigger.start()
     set_timer('session_timer', v.session_duration)
     print('Session started')
-    set_timer('silence_timer', v.target_duration)
+    set_timer('timeout_timer', v.target_duration)
  
 def run_end():
     hw.speaker.off()
@@ -62,7 +62,7 @@ def run_end():
     hw.cameraTrigger.stop()
     print('Session ended. Correct trials: {}/{}'.format(v.correct_trials, v.total_trials))
  
-def wait_for_trial(event):
+def intertrial(event):
     if event == 'entry':
         hw.speaker.off()
         timed_goto_state('stimulus_on', v.iti_duration)
@@ -70,9 +70,9 @@ def wait_for_trial(event):
             v.trial_in_block = 0
             shuffle_list(v.trial_types)  # Shuffle order of next block
     elif event == 'lick':
-        reset_timer('silence_timer', v.target_duration)
-    elif event == 'silence_timer':
-        goto_state('silence')
+        reset_timer('timeout_timer', v.target_duration)
+    elif event == 'timeout_timer':
+        goto_state('timeout')
 
 def stimulus_on(event):
     if event == 'entry':
@@ -103,23 +103,23 @@ def reward_freq (event):
     elif event == 'stimulus_timer':
         hw.speaker.off()
         v.trial_in_block += 1  # Move to next trial in block
-        goto_state('wait_for_trial')
+        goto_state('intertrial')
 
 def reward(event):
     if event == 'entry':
         hw.reward.release()
         v.correct_trials += 1
-        print('Correct! Trials ({}): {}/{}'.format(v.trial_type,v.correct_trials, v.total_trials))
-        timed_goto_state('wait_for_trial', 1000)
-        reset_timer('silence_timer', v.target_duration)
+        print('reward number {}/{} ({})'.format(v.correct_trials, v.total_trials, v.trial_type))
+        timed_goto_state('intertrial', 1000)
+        reset_timer('timeout_timer', v.target_duration)
         v.trial_in_block += 1  # Move to next trial in block
 
-def silence(event):
+def timeout(event):
     if event == 'entry':
         hw.speaker.off()
-        timed_goto_state('wait_for_trial', v.silence_duration)
+        timed_goto_state('intertrial', v.timeout_duration)
     elif event == 'exit':
-        reset_timer('silence_timer', v.target_duration)
+        reset_timer('timeout_timer', v.target_duration)
  
 def all_states(event):
     if event == 'session_timer':
