8771fdf735487618d2d4b179a1b1882157b2d251
gen21kawa <genji.kawakita@gmail.com>
2025-07-31 16:04:58 +0100

    updated hardware definition

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -3,7 +3,7 @@ from devices import *
 board = Breakout_dseries_1_6()
 
 # Instantiate Devices.
-bci_link = UARTlink('bci_update', timer_freq=100)
+bci_link = UARTlink('cursor_update', timer_freq=100)
 
 motionSensor = MotionDetector(name='MotSen1', event='motion',
                               reset=board.port_1.DIO_C,
@@ -14,10 +14,11 @@ motionSensor = MotionDetector(name='MotSen1', event='motion',
 light = LEDStim()
 light.all_off()
 
-lickometer = Lickometer(lick_port=board.port_6, sol_port=board.port_7, rising_event_A='lick', debounce=5)
+lickometer = Lickometer(lick_port=board.port_6, sol_port=board.port_4, rising_event_A='lick', debounce=5)
 reward = Reward(lickometer.SOL_1 , reward_duration=50)
 
-sound = AudioStim(board.port_11)
+sound = AudioStim(board.port_11) # Audio player for sound stimul
+speaker = Speaker(board.port_7) # Speaker (Audio board) for frequency feedback
 
 cameraTrigger = CameraPulse(pin=board.port_1.POW_B, trigger_rate=100, duty_cycle=50)
 
