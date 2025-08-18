6d4a87c15da618bc2976bc0c51919457835b31ab
Mostafa Safaie <mostafa.safaie@gmail.com>
2025-08-18 16:27:56 +0100

    add led-uart

--- devices/UARTlink.py
+++ devices/UARTlink.py
@@ -6,12 +6,17 @@ from machine import Timer
 class UARTlink(IO_object):
     def __init__(self, name, timer_freq = 100):
         """
-        UART device class for Port12
-        name: the framework Event name
+        uart device class: for BCI comm and for led strip
+        whatever integer is recieved from the BCI computer, is passed to the LED strip using the `uart_led`
+        name: the framework Event name for BCI cursor changing value
         timer_freq: int, frequency of the timer, twice the client frequency
         """
-        self.uart = UART(1, 9600)  # uart1=port 12, init with given baudrate
-        self.uart.init(9600, bits=8, parity=None, stop=1)
+        self.uart_bci = UART(1, 9600)  # uart1=port 12, init with given baudrate
+        self.uart_led = UART(4, 9600)  # uart1=port 10, init with given baudrate
+        
+        self.uart_bci.init(9600, bits=8, parity=None, stop=1)
+        self.uart_led.init(9600, bits=8, parity=None, stop=1)
+
         self.buffer = bytearray(8)
         self.name = name
         self.timer_freq = timer_freq
@@ -22,17 +27,22 @@ class UARTlink(IO_object):
         self.prev_spk = 0
 
     def _timer_ISR(self, t):
-        if self.uart.any() > 0:  # there is a message
-            self.uart.readinto(self.buffer, 2)
+        if self.uart_bci.any() > 0:  # there is a message
+            self.uart_bci.readinto(self.buffer, 2)
             self.spk = int.from_bytes(self.buffer, 'little')
             if self.spk != self.prev_spk:
                 self.timestamp = fw.current_time
                 interrupt_queue.put(self.ID)
+                self.uart_led.write(self.buffer)
                 self.prev_spk = self.spk
 
     def _initialise(self):
-        self.timer.init(freq=self.timer_freq)   # this should be 2*(client frequency)
+        self.timer.init(freq=self.timer_freq)
         self.timer.callback(self._timer_ISR)
 
     def _process_interrupt(self):
         fw.event_queue.put((self.timestamp, fw.event_typ, fw.events[self.name]))
+
+    def send_int(self, value: int) -> None:
+        """Send a 2-byte little-endian integer to the host."""
+        self.uart_bci.write(value.to_bytes(2, 'little'))
