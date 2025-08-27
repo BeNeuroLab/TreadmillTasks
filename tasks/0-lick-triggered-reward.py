ac868504733d64b63934627ab33f94adee6f7019
Mostafa Safaie <mostafa.safaie@gmail.com>
2025-08-27 14:14:40 +0100

    update for led strip

--- tasks/0-lick-triggered-reward.py
+++ tasks/0-lick-triggered-reward.py
@@ -8,12 +8,14 @@ import utime
 
 # -------------------------------------------------------------------------
 states = ['trial',
-          'intertrial',
-          'reward']
+    'intertrial',
+    'reward'
+]
 
 events = ['lick',
-          'session_timer',
-          'motion']
+    'session_timer',
+    'motion'
+]
 
 initial_state = 'trial'
 
@@ -26,20 +28,18 @@ v.reward_number = 0
 v.trial_len = 3 * second
 v.led_len = 500 * ms
 
-v.spks___ = [3]
-v.leds___ = [3]
+v.leds___ = [100]
 
 
 # -------------------------------------------------------------------------
 def run_start():
     "Code here is executed when the framework starts running."
-    hw.sound.set_volume(5)  # Between 1 - 30
-    utime.sleep_ms(20)  # wait for the sound player to be ready
     hw.reward.reward_duration = v.reward_duration
     hw.motionSensor.record()
     hw.motionSensor.threshold = 10
-    hw.sound.start()
-    hw.light.all_off()
+    hw.light.start()
+    utime.sleep_ms(20)  # wait for the light
+    hw.light.all_red()
     set_timer('session_timer', v.session_duration, True)
     print('{}, CPI'.format(hw.motionSensor.sensor_x.CPI))
     print('{}, before_camera_trigger'.format(get_current_time()))
@@ -48,11 +48,11 @@ def run_start():
 def run_end():
     "Code here is executed when the framework stops running."
     hw.light.all_off()
+    hw.light.off()
     hw.reward.stop()
     hw.motionSensor.off()
     hw.motionSensor.stop()
     hw.cameraTrigger.stop()
-    hw.sound.stop()
     hw.off()
 
 
@@ -61,12 +61,9 @@ def trial(event):
     "led at first, and spk update at later bins"
     if event == 'entry':
         hw.light.all_off()
-        hw.sound.all_off()
     elif event == 'lick':  # lick during the trial delays the sweep
         hw.light.cue(v.leds___[0])
-        print('{}, led_direction'.format(hw.light.active[0]))
-        hw.sound.cue(v.spks___[0])
-        print('{}, spk_direction'.format(hw.sound.active[0]))
+        print('{}, led_direction'.format(v.leds___[0]))
         timed_goto_state('reward', v.led_len)
 
 def reward (event):
