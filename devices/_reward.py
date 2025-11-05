cad1037e652999adc3f11a6fda582a9a3c6a90df
Mostafa Safaie <mostafa.safaie@gmail.com>
2025-11-05 16:21:41 +0000

    add `sync` to reward

--- devices/_reward.py
+++ devices/_reward.py
@@ -3,13 +3,18 @@ import pyControl.hardware as _h
 
 class Reward(_h.IO_object):
     "Open the solnoid to release water reward"
-    def __init__(self, sol:_h.Digital_output, reward_duration: int=100):
-        "reward_duration in ms"
+    def __init__(self, sol:_h.Digital_output, sync:_h.Digital_output, reward_duration: int=100):
+        """
+        reward_duration in ms
+        self.sync_to_reward is a digital output that goes high when reward is being delivered
+                            copying reward state to sync with pyPhotometry    
+        """
         self.timer = pyb.Timer(_h.available_timers.pop())
-        
         self.reward_duration = reward_duration
+        self.sync_to_reward  = _h.Digital_output(sync)
         self.sol = sol
         self.sol.off()
+        self.sync_to_reward.off()
         self.sol_state = False  # to avoid turning on the solenoid twice
             
     @property
@@ -27,6 +32,7 @@ class Reward(_h.IO_object):
         if self.sol_state is False:
             self.sol.on()
             self.sol_state = True
+            self.sync_to_reward.on()
             self.timer.counter(0)
             self.timer.callback (self._callback)
 
@@ -34,6 +40,7 @@ class Reward(_h.IO_object):
         "turn off the sol"
         self.sol.off()
         self.sol_state = False
+        self.sync_to_reward.off()
         self.timer.callback(None)
     
     def stop(self):
@@ -41,3 +48,4 @@ class Reward(_h.IO_object):
         self.timer.deinit() # stop the timer
         self.sol.off()
         self.sol_state = False
+        self.sync_to_reward.off()
