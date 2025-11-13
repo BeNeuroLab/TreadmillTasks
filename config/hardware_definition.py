984588fd37e28afa968470b59e6bd031e4036185
Martin Esparza <martinesparza22.me@gmail.com>
2025-11-13 12:02:48 +0000

    added equake perturbations

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -3,28 +3,33 @@ from devices import *
 board = Breakout_dseries_1_6()
 
 # Instantiate Devices.
-bci_link = UARTlink('cursor_update', timer_freq=100)
+bci_link = UARTlink("cursor_update", timer_freq=100)
 
 motionSensor = MotionDetector(
-    name='MotSen1', 
-    event='motion',
+    name="MotSen1",
+    event="motion",
     reset=board.port_1.DIO_C,
     cs1=board.port_2.DIO_A,
     cs2=board.port_2.DIO_B,
     calib_coef=1,
-    threshold=1, 
-    sampling_rate=100
+    threshold=1,
+    sampling_rate=100,
 )
 
 light = LedStrip()
 
-lickometer = Lickometer(lick_port=board.port_6, sol_port=board.port_4, rising_event_A='lick', debounce=5)
+lickometer = Lickometer(
+    lick_port=board.port_6, sol_port=board.port_4, rising_event_A="lick", debounce=5
+)
 reward = Reward(sol=lickometer.SOL_1, sync=board.port_6.DIO_B, reward_duration=50)
 
-sound = AudioStim(board.port_11) # Audio player for sound stimul
-speaker = Speaker(board.port_7) # Speaker (Audio board) for frequency feedback
+sound = AudioStim(board.port_11)  # Audio player for sound stimul
+speaker = Speaker(board.port_7)  # Speaker (Audio board) for frequency feedback
 
 cameraTrigger = CameraPulse(pin=board.port_1.POW_B, trigger_rate=100, duty_cycle=50)
+
+# Earthquake
+earthquake_stim = shakeStim(port_exp=Port_expander(port=hw.board.port_8))
 
 # _sync_output = Rsync(pin=board.port_1.POW_A, event_name='rsync', mean_IPI=5000, pulse_dur=50)
 LEDStim().all_off()
---
+++ devices/shakeStim.py
@@ -0,0 +1,39 @@
+import pyb, machine, time
+import pyControl.hardware as _h
+
+
+class shakeStim:
+    "Earthquake machine stimuli."
+
+    def __init__(self, port_exp):
+        self.kill_switch = _h.Digital_output(pin=port_exp.port_1.DIO_B)
+
+        self.sol_0 = _h.Digital_output(pin=port_exp.port_6.DIO_A)
+        self.sol_1 = _h.Digital_output(pin=port_exp.port_6.POW_A, inverted=True)
+        self.sol_2 = _h.Digital_output(pin=port_exp.port_6.POW_B, inverted=True)
+        self.sol_3 = _h.Digital_output(pin=port_exp.port_3.DIO_A)
+        self.sol_4 = _h.Digital_output(pin=port_exp.port_3.DIO_B)
+        self.sol_5 = _h.Digital_output(pin=port_exp.port_3.POW_A, inverted=True)
+        self.sol_6 = _h.Digital_output(pin=port_exp.port_3.POW_B, inverted=True)
+
+        self.sol_7 = _h.Digital_output(pin=port_exp.port_4.DIO_A)
+        self.sol_8 = _h.Digital_output(pin=port_exp.port_4.DIO_B)
+        self.sol_9 = _h.Digital_output(pin=port_exp.port_4.POW_A, inverted=True)
+        self.sol_10 = _h.Digital_output(pin=port_exp.port_4.POW_B, inverted=True)
+
+        self.sol_11 = _h.Digital_output(pin=port_exp.port_1.DIO_A)
+
+    def all_off(self):
+        "turn off all sols"
+        for d in range(self.n_directions):
+            sol = "sol_" + str(d)
+            getattr(self, sol).off()
+
+    def sol_on(self, direction: int):
+        "turn on the sol corresponding to the given direction"
+        sol = "sol_" + str(direction)
+        getattr(self, sol).on()
+
+    def sol_off(self, direction: int):
+        sol = "sol_" + str(direction)
+        getattr(self, sol).off()
